14f2d7f1776fba3cd026fcb279bb9993d22bac4a
JooYoo <jooyoo@outlook.com>
2019-02-26 17:24:54 +0100

    added point by css

--- script/train.js
+++ script/train.js
@@ -1,4 +1,7 @@
 var video = document.getElementById('video');
+var container = document.getElementById('container');
+var leftWrist = document.createElement("div");
+var rightWrist = document.createElement("div");
 let poseNet;
 let poses = [];
 let skeletons = [];
@@ -15,13 +18,17 @@ if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     poseNet.on('pose', function (results) {
       poses = results;
       console.log(poses)
+      drawKeypoints();
     });
-
-    drawKeypoints();
     drawSkeleton();
+
   });
 }
 
+// function setup() {
+//   createCanvas(440, 380);
+//   stroke(255);
+// }
 
 
 // A function to draw ellipses over the detected keypoints
@@ -32,11 +39,50 @@ function drawKeypoints() {
     for (let j = 0; j < poses[i].pose.keypoints.length; j++) {
       // A keypoint is an object describing a body part (like rightArm or leftShoulder)
       let keypoint = poses[i].pose.keypoints[j];
+
+
       // Only draw an ellipse is the pose probability is bigger than 0.2
-      if (keypoint.score > 0.2) {
-        fill(255, 0, 0);
-        noStroke();
-        ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
+      if (keypoint.score > 0.5) {
+        // fill(255, 0, 0);
+        // noStroke();
+        // ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
+
+        // draw point
+        switch (keypoint.part) {
+          case "leftWrist":
+            leftWrist.style.width = "15px";
+            leftWrist.style.height = "15px";
+            leftWrist.style.left = (keypoint.position.x).toString() + "px";
+            leftWrist.style.top = (keypoint.position.y).toString() + "px";
+            leftWrist.style.backgroundColor = "red";
+            leftWrist.style.position = "absolute";
+            leftWrist.style.zIndex = "100";
+            container.appendChild(leftWrist);
+            break;
+
+          case "rightWrist":
+            rightWrist.style.width = "15px";
+            rightWrist.style.height = "15px";
+            rightWrist.style.left = (keypoint.position.x).toString() + "px";
+            rightWrist.style.top = (keypoint.position.y).toString() + "px";
+            rightWrist.style.backgroundColor = "blue";
+            rightWrist.style.position = "absolute";
+            rightWrist.style.zIndex = "100";
+            container.appendChild(rightWrist);
+            break;
+
+          default:
+            break;
+        }
+
+        // try {
+        //   container.removeChild(leftWrist);
+        //   container.removeChild(rightWrist);
+        // } catch (error) {
+          
+        // }
+       
+        console.log("point");
       }
     }
   }
